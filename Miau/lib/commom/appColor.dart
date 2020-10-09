096a38e10a666ecabfc09ed93857c8ccf5b42032
José Matias Lemes Filho <matiasfilho@gmail.com>
2020-10-08 22:07:09 -0300

    criacao do card da tela de home

---
+++ Miau/lib/commom/appColor.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors();
+
+  // texto
+  static const Color text = Colors.white;
+
+  // botão
+  static const Color bottons = Colors.green;
+
+  // card principal
+  static const Color cardSlide = const Color(0xFFFFFFFF);
+
+  // fundo do programa
+  static const Color backgroud = const Color(0xFFFFA726);
+
+  static const Color secundary = const Color(0xFF597EFF);
+  static const Color backgndAvatar = Colors.amber;
+}
--- Miau/lib/main.dart
+++ Miau/lib/main.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'commom/appColor.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,15 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,59 +34,40 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Container(
+          color: AppColors.backgroud,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              card(),
+            ],
+          )
+
+          // Center(
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       Text(
+          //         'You have pushed the button this many times:',
+          //       ),
+          //       Text(
+          //         '$_counter',
+          //         style: Theme.of(context).textTheme.headline4,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
@@ -114,4 +75,94 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget card() {
+  return Container(
+    color: AppColors.cardSlide,
+    width: 340.0,
+    height: 340.0,
+    child: Column(
+      children: [
+        //linha
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Container(
+                  color: AppColors.secundary,
+                  width: 40.0,
+                  height: 40.0,
+                ),
+                Text("Oi Rapha")
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  color: AppColors.secundary,
+                  width: 40.0,
+                  height: 40.0,
+                ),
+                Text("Oi Rapha")
+              ],
+            )
+          ],
+        ),
+
+        //imagem
+        Container(
+          decoration: BoxDecoration(
+            color: AppColors.cardSlide,
+            image: const DecorationImage(
+              image: NetworkImage(
+                  'https://i.guim.co.uk/img/media/684c9d087dab923db1ce4057903f03293b07deac/205_132_1915_1150/master/1915.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=14a95b5026c1567b823629ba35c40aa0'),
+              fit: BoxFit.cover,
+            ),
+            border: Border.all(
+              color: AppColors.cardSlide,
+              width: 8,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 320.0,
+          height: 220.0,
+        ),
+
+        // baixo
+        Row(
+          children: [listadeICones(), listadeShare()],
+        )
+      ],
+    ),
+  );
+}
+
+Widget listadeShare() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: const <Widget>[
+      Icon(
+        Icons.share,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+      ),
+    ],
+  );
+}
+
+Widget listadeICones() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: const <Widget>[
+      Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+      ),
+    ],
+  );
 }
